69f0b5105d84f7c6dc3f496942eb00ffe292de4d
Petr-max <petrpuzaev@gmail.com>
2023-01-19 18:20:14 +0300

    все целые числа в

---
+++ Preset005/Program.cs
@@ -0,0 +1,23 @@
+﻿// Работа в группе:
+// напишите программу, которая на вход
+// принимает одно число (N), а на выходе
+// показывает все целые числа в
+// промежутке от -N до N.
+// 4 -> " -4, -3, -2, -1, 0, 1, 2, 3, 4."
+// 2 -> " -2, -1, 0, 1, 2."
+
+Console.WriteLine("Введите число: ");
+int namber = Convert.ToInt32(Console.ReadLine());
+if (namber > 0)
+{
+  int count = -namber;
+  while (count <= namber)
+  {
+    Console.Write(count + " ");
+    count++;
+  }
+}
+else
+{
+  Console.WriteLine("Число должно быть больше нуля: ");
+}
